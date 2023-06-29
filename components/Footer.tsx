beb2be20140d6ac211585082cac118c98e6ad047
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2023-06-29 15:23:32 +0600

    footer done

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -73,6 +73,13 @@ const Footer = ({}) => {
           </div>
         </div>
       </div>
+      <div className="flexBetween footer_copyright">
+        <p>@ 2023 Flexibble. All rights reserved.</p>
+        <p className="text-gray">
+          <span className="text-black font-semibold">10,214</span> projects
+          submitted
+        </p>
+      </div>
     </footer>
   );
 };
