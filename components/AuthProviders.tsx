803a6bc0f40bd95a0fb336a7a48c84b90ab44e78
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2023-06-29 17:44:30 +0600

    graphql init

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,9 +1,44 @@
-import { FC } from "react";
+"use client";
+
+import { FC, useState, useEffect } from "react";
+import { getProviders, signIn } from "next-auth/react";
 
 interface AuthProvidersProps {}
 
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | null;
+};
+
+type Providers = Record<string, Provider>;
+
 const AuthProviders: FC<AuthProvidersProps> = ({}) => {
-  return <div>AuthProviders</div>;
+  const [providers, setProviders] = useState<Providers | null>(null);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
+
+      setProviders(res);
+    };
+    fetchProviders();
+  }, []);
+
+  if (providers) {
+    return (
+      <div>
+        {Object.values(providers).map((provider: Provider, i) => (
+          <button onClick={() => signIn(provider?.id)} key={i}>
+            {provider.id}
+          </button>
+        ))}
+      </div>
+    );
+  }
 };
 
 export default AuthProviders;
